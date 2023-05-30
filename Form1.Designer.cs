68d753e97e4f3696cb95232e4c751e64cf005a64
abish <abish@>
2023-05-30 20:06:40 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Rotateimages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(42, 379);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 29);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "Start";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 325);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rotateimages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnProcess_Click(object sender, EventArgs e)
+        {
+           
+                var folderDialog = new FolderBrowserDialog();
+                var result = folderDialog.ShowDialog();
+
+             
+                if (result == DialogResult.OK)
+                {
+                    var sourceFolder = folderDialog.SelectedPath;
+                    var targetFolder = Path.Combine(sourceFolder, "RotatedImages");
+
+                    if (!Directory.Exists(targetFolder))
+                    {
+                        Directory.CreateDirectory(targetFolder);
+                    }
+
+                    var reportFile = Path.Combine(sourceFolder, "report.txt");
+
+                    using (var writer = new StreamWriter(reportFile))
+                    {
+                        var files = Directory.GetFiles(sourceFolder, "*.jpg");
+                        var tasks = new List<Task>();
+                        var threads =new List<Thread>();
+                        var startTime = DateTime.Now;
+
+
+                    //РЕШЕНИЕ С ПОМОЩЬЮ THREAD
+
+                        foreach (var file in files)
+                        {
+                            var thread = new Thread(() =>
+                            {
+                                var image = Image.FromFile(file);
+                                image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                                var targetFile = Path.Combine(targetFolder, Path.GetFileName(file));
+                                image.Save(targetFile);
+                                var elapsedTime = (DateTime.Now - startTime).TotalMilliseconds;
+                                writer.WriteLine($"{file}\n\t Thread отработал за{elapsedTime:0.000} ms");
+                            });
+
+                            thread.Start();
+                            threads.Add(thread);
+
+                        }
+
+                        foreach (var thread in threads)
+                        {
+                            thread.Join();
+                        }
+
+
+
+
+                       // Решение с помощью Parallel
+                        Parallel.ForEach(files, file =>
+                        {
+                            var image = Image.FromFile(file);
+                            image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                            var targetFile = Path.Combine(targetFolder, Path.GetFileName(file));
+                            image.Save(targetFile);
+                            var elapsedTime = (DateTime.Now - startTime).TotalMilliseconds;
+                            writer.WriteLine($"{file}\n\t Parallel отработал за{elapsedTime:0.000} ms");
+
+                        });
+
+
+
+                        //Решение с помощью TASK
+                        foreach (var file in files)
+                        {
+                            var task = Task.Run(() =>
+                            {
+                                var image = Image.FromFile(file);
+                                image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                                var targetFile = Path.Combine(targetFolder, Path.GetFileName(file));
+                                image.Save(targetFile);
+                                var elapsedTime = (DateTime.Now - startTime).TotalMilliseconds;
+                                writer.WriteLine($"{file}\n\t Task отработал за {elapsedTime:0.000}  ms");
+                            });
+
+                            tasks.Add(task);
+                        }
+
+                        await Task.WhenAll(tasks);
+                    }
+
+                        MessageBox.Show("Process completed!");
+                        // читаем содержимое файла report.txt
+                        string[] lines = File.ReadAllLines(reportFile);
+
+                        // выводим результат в textBox
+                        textBox1.Lines = lines;
+                }
+
+            
+        }
+    }
+}
